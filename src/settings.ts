bbc7a5ff1a8f1f39aa32c164440435e166fe34d8
zol22 <zol2295@live.com>
2025-04-23 18:53:02 -0400

    fix sitemap

--- src/settings.ts
+++ src/settings.ts
@@ -20,7 +20,7 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // 'https://solangeormeno.dev' Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://solangeormeno.dev', // 'https://solangeormeno.dev' Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
